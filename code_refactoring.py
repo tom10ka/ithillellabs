dd991ee3e5189d0ab890bbc0bf806086177e8013
Trofimov Viacheslav <trofimov.slava19@gmail.com>
2023-05-03 17:42:18 +0300

    homework is done

--- code_refactoring.py
+++
@@ -1,7 +0,0 @@
-# очікуваний результат у вигляді: My name is David, I am 14 years old👣
-
-smile_footprint = '\U0001F463'
-user_name = input('Please, enter your name >>> ').capitalize().strip()
-user_age = input('Please, enter your age >>> ').strip()
-result = f'My name is {user_name}, I am {user_age}{smile_footprint}'
-print(result)
