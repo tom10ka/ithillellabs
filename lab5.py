61b42eb85e98e720c95e5b4f0415557eab6b562f
Trofimov Viacheslav <trofimov.slava19@gmail.com>
2023-05-03 17:30:33 +0300

    homework #5

---
+++ lab5.py
@@ -0,0 +1,8 @@
+user_name = input('Введіть ваше імя ').capitalize().strip('1234567890')
+user_surname = input('Введіть ваше прізвище ').upper().strip('1234567890')
+name_length = len(user_name)
+template = 'Привіт, {user_name} {user_surname}, а ти знав, що твоє імя складається з {name_length} літер'
+user_info = template.format(user_name=user_name, user_surname=user_surname, name_length=name_length)
+template_new = user_info.replace('Привіт', 'Здарова')
+change4 = f'{template_new}?'
+print(change4)
