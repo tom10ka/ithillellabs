a836766cca95339008a363a5bb51255a5d4a3332
Trofimov Viacheslav <trofimov.slava19@gmail.com>
2023-05-03 17:20:08 +0300

    homework is done

--- code_refactoring.py
+++ code_refactoring.py
@@ -1,9 +1,7 @@
 # очікуваний результат у вигляді: My name is David, I am 14 years old👣
 
-# example
-# f = '\N{Footprints}'  # not informative naming, the correct code below
 smile_footprint = '\U0001F463'
-user_name = input().capitalize()
-user_age = input('Please, enter your age >>> ')
+user_name = input('Please, enter your name >>> ').capitalize().strip()
+user_age = input('Please, enter your age >>> ').strip()
 result = f'My name is {user_name}, I am {user_age}{smile_footprint}'
 print(result)
