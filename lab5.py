b5817464d4bb53541558f329c51dea732aaf62cc
Trofimov Viacheslav <trofimov.slava19@gmail.com>
2023-05-04 17:27:04 +0300

    homework reworked

--- lab5.py
+++ lab5.py
@@ -1,5 +1,5 @@
-user_name = input('Введіть ваше імя ').capitalize().strip('1234567890')
-user_surname = input('Введіть ваше прізвище ').upper().strip('1234567890')
+user_name = input('Введіть ваше імя ').capitalize().strip('1234567890 ')
+user_surname = input('Введіть ваше прізвище ').upper().strip('1234567890 ')
 name_length = len(user_name)
 template = 'Привіт, {user_name} {user_surname}, а ти знав, що твоє імя складається з {name_length} літер'
 user_info = template.format(user_name=user_name, user_surname=user_surname, name_length=name_length)
