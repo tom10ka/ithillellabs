7408dfed5657b5fc2ca167f5c66391a6f5d79f8e
Trofimov Viacheslav <trofimov.slava19@gmail.com>
2023-05-21 11:46:23 +0300

    changed

--- lab9.py
+++ lab9.py
@@ -7,35 +7,28 @@ for list_character in numbers_list:
 print(new_numbers_list)
 
 # 2
-surnames_list = []
-new_surname = input('Write pupils surname, input "stop" when you finish >>> ').capitalize()
-if new_surname != 'Stop':
-    surnames_list.append(new_surname)
-    for new_surname in surnames_list:
-        if new_surname != 'Stop':
-            new_surname = input('Write pupils surname >>> ').capitalize()
-            surnames_list.append(new_surname)
-        else:
-            surnames_list.remove('Stop')
-            surnames_list.sort()
-            print(surnames_list)
-            break
-else:
-    print('List is empty')
+new_surname = input('Write pupils surnames using space >>> ')
+new_list = new_surname.split()
+new_list_2 = []
+for capital_letter in new_list:
+    capital_letter = capital_letter.capitalize()
+    new_list_2.append(capital_letter)
+new_list_2 = sorted(new_list_2)
+print(new_list_2)
 
 # 3
 letters_list = str(input('Write down letters and numbers order >>> '))
-letters_list_length = len(letters_list)
-right_letters_list = ''
-for letters_list_length in letters_list:
-    if letters_list_length.isupper() == True:
-        right_letters_list = right_letters_list + letters_list_length
+symbol = len(letters_list)
+right_letters_string = ''
+for symbol in letters_list:
+    if symbol.isupper() == True:
+        right_letters_string = right_letters_string + symbol
     else:
         continue
-if len(right_letters_list) == 0:
+if len(right_letters_string) == 0:
     print('String is empty, there are no proper characters')
 else:
-    print(str(right_letters_list))
+    print(str(right_letters_string))
 
 # 4
 from decimal import Decimal
