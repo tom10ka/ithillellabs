d3f7095f34232f34c0a9d365e6794df4d55e44b7
Trofimov Viacheslav <trofimov.slava19@gmail.com>
2023-05-27 00:51:27 +0300

    homework is done

--- lab12.py
+++ lab12.py
@@ -1,34 +1,21 @@
 import requests
 
-url = 'https://dummyjson.com/users?limit=100'
+url = 'https://script.googleusercontent.com/macros/echo?user_content_key=g--wIIvZzzhjImsmdqn5WXq82Dg-X-_XEf0KSp4PX_NjyrtFEMl8zI5kclide4eS6nlI30vVUM8bxAyMP8Y2c1Hikm9EUYd2m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnJJSz-0qgNnrjX_lsmjyF66AquHjXmDhbua3x6VUlwrMS4HaXlKRjvWwrQxiYFxAdff-H-FPGOuEUWsSDVQYINaFCpdjScVu7A&lib=MePDWmZU7aIdRiHQVCmZnzjUevPycBIIq'
 
 response = requests.get(url)
-data = response.json()
-users = data['users']
-brownheaded_number = 0
-users_general_age = 0
-users_from_lousville = []
-for user in users:
-    user_hair = user['hair']
-    user_hair_color = user_hair.get('color', 0)
-    user_address = user['address']
-    user_city = user_address.get('city', 0)
-    if user_city == 'Louisville':
-        user_first_name = user.get('firstName', 'Noname')
-        user_last_name = user.get('lastName', 'Nolastname')
-        whole_name = f'{user_first_name} {user_last_name}'
-        users_from_lousville.append(whole_name)
-    if user_hair_color == 'Brown':
-        brownheaded_number += 1
-        user_age = user['age']
-        users_general_age += user_age
-if brownheaded_number > 0:
-    average_age = users_general_age / brownheaded_number
-    print(average_age)
-else:
-    print('There are no brownheaded persons')
-users_from_lousville_number = len(users_from_lousville)
-if users_from_lousville_number > 0:
-    print(users_from_lousville)
-else: 
-    print('There are no humans from Louisville')
+inner_data = response.json()
+clean_data = inner_data['data']
+whole_price = 0
+product_price = 0
+product_quantity = 0
+whole_price_no_gluten = 0
+for product in clean_data:
+    product_price = float(product.get('price', 0))
+    product_quantity = float(product.get('quantity', 0))
+    whole_product_price = product_price * product_quantity
+    whole_price += whole_product_price
+    product_gluten = product.get('is_gluten')
+    if product_gluten is False:
+        whole_price_no_gluten += whole_product_price
+print(f'General price of all goods {whole_price}')
+print(f'General price of all non-gluten goods {whole_price_no_gluten}')
