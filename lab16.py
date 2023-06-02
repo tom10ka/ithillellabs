4a79717955ebc1df28afb800bc1e7e6c712727dc
Trofimov Viacheslav <trofimov.slava19@gmail.com>
2023-06-03 00:17:16 +0300

    reworked

--- lab16.py
+++ lab16.py
@@ -1,18 +1,19 @@
 def month_calendar():
-    month_list = [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-        'July',
-        'August',
-        'September',
-        'October',
-        'November',
-        'December',
-    ]
-    for month in month_list:
-        month_list.append(month)
-        yield month
+    while True:
+        month_list = [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December',
+        ]
+        for month in month_list:
+            yield month
+            continue
