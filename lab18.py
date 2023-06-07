991a0035e080c709f88bf6721078de8385486f2e
Trofimov Viacheslav <trofimov.slava19@gmail.com>
2023-06-08 00:00:12 +0300

    rework

--- lab18.py
+++ lab18.py
@@ -1,17 +1,8 @@
-from typing import Any
-
-
 def string_refactor(func):
     def wrapper(*args, **kwargs):
         result = func(*args, **kwargs)
         if type(result) is str:
             result = f'<b>{result}</b>'
         return result
+
     return wrapper
-
-
-
-
-
-
-
