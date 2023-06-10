82cb1f9b1ddadf8d811230524afe481dd144ba11
Trofimov Viacheslav <trofimov.slava19@gmail.com>
2023-06-10 23:33:07 +0300

    homework is done

--- lab18.py
+++
@@ -1,8 +0,0 @@
-def string_refactor(func):
-    def wrapper(*args, **kwargs):
-        result = func(*args, **kwargs)
-        if type(result) is str:
-            result = f'<b>{result}</b>'
-        return result
-
-    return wrapper
---
+++ lab19.py
@@ -0,0 +1,17 @@
+import csv
+
+with open(file='111.csv', mode='r', encoding='utf-8') as file:
+    reader = csv.DictReader(file, delimiter=';')
+    names_list = []
+    for row in list(reader):
+        value = row.get('iso_country')
+        if value == 'UA':
+            airport_name = row.get('name')
+            names_list.append(airport_name)
+    new_str = ''
+    for name in names_list:
+        if name != names_list[-1]:
+            new_str += f'{name}, '
+        else:
+            new_str += f'{name}.'
+    print(new_str)
